bd8011e8dee17107e18ec77f784b890fcb704b86
AlekseyLeshko <alekseyleshko@gmail.com>
2019-10-12 17:03:06 +0300

    Fix test

--- src/__tests__/sum.test.js
+++ src/__tests__/sum.test.js
@@ -4,6 +4,6 @@ test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
 
-test('adds 1 + 4 to equal 3', () => {
-  expect(sum(1, 4)).toBe(3);
+test('adds 1 + 4 to equal 5', () => {
+  expect(sum(1, 4)).toBe(5);
 });
