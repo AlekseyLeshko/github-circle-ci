33e041fc816777ffd36101b4227dd5908b514e30
AlekseyLeshko <alekseyleshko@gmail.com>
2019-10-15 00:16:11 +0300

    Add test

--- src/__tests__/sum-3.test.js
+++ src/__tests__/sum-3.test.js
@@ -7,3 +7,7 @@ test('adds 1 + 2 to equal 3', () => {
 test('adds 1 + 4 to equal 5', () => {
   expect(sum(1, 4)).toBe(5);
 });
+
+test('adds 3 + 3 to equal 6', () => {
+  expect(sum(3, 3)).toBe(6);
+});
